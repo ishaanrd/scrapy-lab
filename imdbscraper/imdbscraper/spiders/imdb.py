debe6d2e845e4c5b30234c2bf7a474aa2119e6ff
ishaanrd <42520918+ishaanrd@users.noreply.github.com>
2020-04-16 23:59:48 +0200

    added parse_bio calls and connection to another elastic index

--- imdbscraper/imdbscraper/spiders/imdb.py
+++ imdbscraper/imdbscraper/spiders/imdb.py
@@ -1,23 +1,36 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import re
+from elasticsearch import Elasticsearch
+import os
+from uuid import uuid4
+
+ELASTIC_API_URL_HOST = os.environ['ELASTIC_API_URL_HOST']
+ELASTIC_API_URL_PORT = os.environ['ELASTIC_API_URL_PORT']
+ELASTIC_API_USERNAME = os.environ['ELASTIC_API_USERNAME']
+ELASTIC_API_PASSWORD = os.environ['ELASTIC_API_PASSWORD']
+
+es=Elasticsearch(host=ELASTIC_API_URL_HOST,
+                 scheme='https',
+                 port=ELASTIC_API_URL_PORT,
+                 http_auth=(ELASTIC_API_USERNAME,ELASTIC_API_PASSWORD))
 
 class ImdbSpider(scrapy.Spider):
     name = 'imdb'
-    start_urls = ['https://www.imdb.com/title/tt0758758/fullcredits/']
-    #start_urls = ["https://www.imdb.com/name/nm0386472/bio"]
-    scraped_movies = ["/title/tt0758758/"]
-
+    start_urls = ['https://www.imdb.com/title/tt0081505/fullcredits/']
+    scraped_movies = ["/title/tt0081505/"]
+    scraped_actors = []
+    actor_name=""
     def parse(self, response):
         movie_id = response.url.split("/")[4]
-        title_text = response.css("title::text").extract()[0]
-        movie_name = " ".join(title_text.split("-")[0].split(" ")[:-2])
-        movie_year = title_text.split("-")[0].split(" ")[-2][1:-1]
+        #title_text = response.css("title::text").extract()[0]
+        movie_name = response.css('.parent a::text').extract_first()
+        movie_year = response.css('.nobr::text').extract_first().strip()[1:5]
         for actor_table in response.css(".cast_list"):
             actors = actor_table.css(".odd") + actor_table.css(".even")
             for actor in actors:
                 actor_info = actor.css("td")
-                actor_name = actor_info[1].css("::text").extract()[1].strip()
+                self.actor_name = actor_info[1].css("::text").extract()[1].strip()
                 actor_id = actor_info[1].css('a::attr(href)').extract_first().split("/")[2]
                 try:
                     role_name = actor_info[3].css("::text").extract()[1].strip().replace('\n', '')
@@ -27,11 +40,27 @@ class ImdbSpider(scrapy.Spider):
                 yield{
                     "movie_id": movie_id,
                     "movie_name": movie_name,
-                    "movie_year": int(movie_year),
-                    "actor_name": actor_name,
+                    "movie_year": (movie_year),
+                    "actor_name": self.actor_name,
                     "actor_id": actor_id,
                     "role_name": role_name
                 }
+                es.index(index='imdb1_movies',
+                         doc_type='movies',
+                         #id=uiid4(),
+                         body={
+                             "movie_id": movie_id,
+                             "movie_name": movie_name,
+                             "movie_year": (movie_year),
+                             "actor_name": self.actor_name,
+                             "actor_id": actor_id,
+                             "role_name": role_name
+                         })
+
+                if actor_id not in self.scraped_actors:
+                    self.scraped_actors.append(actor_id)
+                    actor_bio = f"https://www.imdb.com/name/{actor_id}/bio/"
+                    yield response.follow(actor_bio,callback=self.parse_bio)
 
                 actor_url = f"https://www.imdb.com/name/{actor_id}/"
                 yield response.follow(actor_url, callback=self.parse_actor)
@@ -61,7 +90,6 @@ class ImdbSpider(scrapy.Spider):
                 film_url = f"https://www.imdb.com{movie_id}fullcredits/"
                 yield response.follow(film_url, callback=self.parse)
 
-
     def parse_bio(self, response):
         table = response.css("#overviewTable")
         birth_year = table.css("tr")[0]
@@ -75,5 +103,11 @@ class ImdbSpider(scrapy.Spider):
         height = height.split("(")[1].split(")")[0][:-2]
         height = float(height)
 
-        print(height)
-        print(birth_year)
+        es.index(index='imdb1_bio',
+                 doc_type='actor_bio',
+                 # id=uiid4(),
+                 body={
+                     "actor_name" : self.actor_name,
+                     "actor_height": float(height),
+                     "actor_age": int(1985-birth_year)
+                 })
